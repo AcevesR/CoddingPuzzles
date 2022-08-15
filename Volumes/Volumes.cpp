9838fae7ba6edbd8aec0640bb52f8a9d7266e7d6
Rodrigo Aceves <rdrg.acvs@gmail.com>
2022-08-14 21:05:21 -0700

    Update Volumes.cpp

--- Volumes/Volumes.cpp
+++ Volumes/Volumes.cpp
@@ -1,63 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 #define PI 3.141592
-float 	pide();
-float area (float r);
-void area(); 
-void imprime (float);
+
+float ask();
+float area(float r);
+void print(float);
 
 main()
 {
-      int dec, elec;
+      int dec, choice;
       float Vci, Vco, r,h, ar;
-      do
-	  {
-		printf("Hola\n");
-      printf("Do you want to calculate the volume of...? 1)Cylinder 2)Cone");
-      scanf("%i",&dec);
-      printf("Write the value of the radius");
-      scanf("%f",&r);
-      ar=area(r);
+
+      do {
+	printf("Hello.\n");
+      printf("Do you want to calculate the volume of...? \n1) Cylinder \n2) Cone\n");
+      scanf("%i", &dec);
+      printf("Write the value of the radius\n");
+      scanf("%f", &r);
+      ar = area(r);
     
-      if(dec==1)
-      {
-        h=pide();
-        Vci=ar*h;
-        imprime(Vci);
-       }
-       else 
-       {
-       	h=pide();
-    	Vco=(Vci)/3;
-        imprime(Vco);
-		}                         
-      printf("Do you want to calculate another volume? 1)Yes 2)No");
-      scanf("%i",&elec);
+      if(dec == 1) {
+        h = ask();
+        Vci = ar * h;
+        print(Vci);
       }
-	  while (elec==1);
-	  system("PAUSE");
+      else {
+      h = ask();
+    	Vco = (Vci) / 3;
+      print(Vco);
+	}   
+
+      printf("Do you want to calculate another volume? \n1)Yes \n2)No\n");
+      scanf("%i", &choice);
+
+      }
+	while (choice == 1);
+      
+	system("PAUSE");
       system("cls");
       printf("A La Gloria Del Gran Arquitecto Del Universo\n");
       system("PAUSE"); 
 }
 
-float pide()
+float ask()
 {
       float h;     
-      printf("Write the value of the height");
-      scanf("%f",&h);
+      printf("Write the value of the height\n");
+      scanf("%f", &h);
       return h;
 }
 
 float area(float r)
 {
      float area2;
-     area2=PI*r*r;
+     area2 = PI * r * r;
      return area2;
 }
 
-void imprime (float Vol)
+void print (float Vol)
 {
      printf("The volume of the figure is %f ", Vol);
  }
